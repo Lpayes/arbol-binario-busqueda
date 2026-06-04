8501914e4e6272fb0ff9ce221147db44cc73740b
Lester David Payes Méndez <lpayesm@miumg.edu.gt>
2026-06-03 20:42:20 -0600

    problema 4 implementar ancestro comun mas bajo

--- arboles/src/main/java/umg/edu/progra/arboles/ArbolBinarioBusqueda.java
+++ arboles/src/main/java/umg/edu/progra/arboles/ArbolBinarioBusqueda.java
@@ -365,6 +365,31 @@ public class ArbolBinarioBusqueda {
         return esBSTValidoRecursivo(nodo.izquierdo, minimo, nodo.dato)
                 && esBSTValidoRecursivo(nodo.derecho, nodo.dato, maximo);
     }
+    
+    public int ancestroComunMasBajo(int a, int b) {
+
+        if (!contiene(a) || !contiene(b)) {
+            throw new IllegalArgumentException(
+                    "Ambos valores deben existir en el arbol");
+        }
+
+        return ancestroComunMasBajoRecursivo(raiz, a, b);
+    }
+
+    private int ancestroComunMasBajoRecursivo(Nodo nodo, int a, int b) {
+
+        if (a < nodo.dato && b < nodo.dato) {
+            return ancestroComunMasBajoRecursivo(
+                    nodo.izquierdo, a, b);
+        }
+
+        if (a > nodo.dato && b > nodo.dato) {
+            return ancestroComunMasBajoRecursivo(
+                    nodo.derecho, a, b);
+        }
+
+        return nodo.dato;
+    }
 
     // ============================================================
     // COLA INTERNA (lista enlazada simple) usada para BFS.
